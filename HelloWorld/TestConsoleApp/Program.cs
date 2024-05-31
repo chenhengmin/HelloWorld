af7f4cfc2922bef2c7ee61bb6c4e2209fa1b7e6d
陈恒敏 <chenhengmin@hotmail.com>
2024-05-31 13:19:08 +0800

    测试 2024年5月31日 13:18:41

--- HelloWorld/TestConsoleApp/Program.cs
+++ HelloWorld/TestConsoleApp/Program.cs
@@ -10,6 +10,7 @@ namespace TestConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
             Console.Read();
         }
 
